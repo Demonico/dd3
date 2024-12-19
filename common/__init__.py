fae930aba79db6d1ed74ef846ad0f41067be3c2b
JVD <jvandoren@gmail.com>
2024-12-18 23:35:10 -0700

    first problem solved

---
+++ common/__init__.py
@@ -0,0 +1,1 @@
+from .example_input import ExampleInput
---
+++ week1/algo/two_sum.py
@@ -0,0 +1,38 @@
+from common import ExampleInput
+
+# https://leetcode.com/problems/two-sum/description/
+class Solution:
+    @staticmethod
+    def two_sum(nums, target):
+        hmap = {}
+
+        for i, num in enumerate(nums):
+            if num in hmap:
+                return [hmap[num], i]
+            hmap[target - num] = i
+        print(hmap.items())
+        return []
+
+
+if __name__ == '__main__':
+    test_case1 = ExampleInput(nums=[2, 7, 11, 15], target=9)
+    test_case2 = ExampleInput(nums=[3,2,4], target=6)
+    test_case3 = ExampleInput(nums=[3,3], target=6)
+    test_case4 = ExampleInput(nums=[-1,-2,-3,-4,-5], target=-8)
+    solution = Solution()
+
+    test_result1 = solution.two_sum(test_case1.nums, test_case1.target)
+    print(test_result1)
+    assert test_result1 == [0,1]
+
+    test_result2 = solution.two_sum(test_case2.nums, test_case2.target)
+    print(test_result2)
+    assert test_result2 == [1,2]
+
+    test_result3 = solution.two_sum(test_case3.nums, test_case3.target)
+    print(test_result3)
+    assert test_result3 == [0,1]
+
+    test_result4 = solution.two_sum(test_case4.nums, test_case4.target)
+    print(test_result4)
+    assert test_result4 == [2,4]
